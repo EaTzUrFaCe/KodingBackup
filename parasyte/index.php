91f32683e9a629498aff0a248e9461e0c8c4c925
Matt Hermes <mathwhiz19@gmail.com>
2015-04-01 13:40:58 +0000

    Initial commit

---
+++ parasyte/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width,initial-scale=1">
+<link rel="stylesheet" href="//koding.com/hello/css/style.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<script src="mediaelement-and-player.min.js"></script>
+<link rel="stylesheet" href="mediaelementplayer.css" />
+<title>Parasyte - The Maxim</title>
+<!--[if IE]>
+  <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+<link href='//fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+</head>
+<body class="php">
+<div id="container">
+  <div id="main" role="main" class="hellobox">
+    <header><a href="http://koding.com">Koding.com</a></header>
+    <h1>Hello World!</h1>
+    <h2>This is my testing development server!</h2>
+  </div>
+  <nav>
+    <ul>
+      <li><a href="/">Index</a></li>
+      <li><a class="active" href="/parasyte/">Parasyte - The Maxim</a></li>
+    </ul>
+  </nav>
+  <footer>
+    <video src="Parasyte-E9.ts" width="1024" height="768"></video>
+  </footer>
+</div>
+</body>
+</html>
+
+
+
+
